4e95add97ef4c9417986efeaabe99d926300c75e
faankim1984 <faankimsmanela@gmail.com>
2023-10-06 13:57:44 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function(){
+Route::get('home', function(){
+    return view('pages.app.dashboard-siakad',['type_menu' => '']);
+    })->name('home');
+Route::resource('user', UserController::class);
+});
+
+// Route::get('/', function () {
+//     return view('pages.auth.auth-login');
+//    })->name('login');
+
+// Route::get('/login', function () {
+//     return view('pages.auth/auth-login', ['type_menu' => '']);
+//    })->name('login');
+
+// Route::get('/register', function () {
+//     return view('pages.auth/auth-register', ['type_menu' => '']);
+//    })->name('register');
+
+// Route::get('/forgot', function () {
+//     return view('pages.auth/auth-forgot-password', ['type_menu' => '']);
+//    })->name('forgot');
+
+// Route::get('/reset', function () {
+//     return view('pages.auth/auth-reset-password', ['type_menu' => '']);
+//    })->name('reset');
